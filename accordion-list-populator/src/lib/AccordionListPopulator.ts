88de80ab72c7e199063d41ef240b522680f8ab31
egadams <31349790+EGAdams@users.noreply.github.com>
2022-08-04 19:18:01 -0400

    cleaned up some dead code

--- accordion-list-populator/src/lib/AccordionListPopulator.ts
+++ accordion-list-populator/src/lib/AccordionListPopulator.ts
@@ -1,5 +1,4 @@
 import { AccordionListPopulatorResultProcessor } from "./AccordionListPopulatorResultProcessor";
-import IApiArgs from "./IApiArgs";
 import OriginalQueryRunner from './OriginalQueryRunner';
 /** @class AccordionListPopulator */
 export class AccordionListPopulator {
@@ -8,19 +7,14 @@ export class AccordionListPopulator {
         this.url = "http://mycustombusinessapp.com/wp-content/plugins/MCBA-Wordpress/runQuery.php";
         console.log( "initializing channel " + url + "..." );
     }
-    populate( ) {
+    populate() {
         const dataSource = new OriginalQueryRunner();
-        dataSource.runQuery( {
+        dataSource.runQuery({
             query: "select object_view_id from monitored_objects;",
             queryResultProcessor: new AccordionListPopulatorResultProcessor(),
             data: {},
             thisObject: this
         });
-    }
-
-    logObjectsLoaded( event: JQuery.Event, apiArgs: IApiArgs ) {
-        console.log( "logObjectsLoaded " + event + " " + apiArgs);
-        console.log( "examine output." );
     }
 }
 
