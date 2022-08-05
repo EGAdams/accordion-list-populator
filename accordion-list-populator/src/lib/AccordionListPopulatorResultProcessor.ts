7c88f8056fb659f00eaddf1444969216351d400e
egadams <31349790+EGAdams@users.noreply.github.com>
2022-08-05 13:07:59 -0400

    the accordion list populator result processor builds a unique array.

--- accordion-list-populator/src/lib/AccordionListPopulatorResultProcessor.ts
+++ accordion-list-populator/src/lib/AccordionListPopulatorResultProcessor.ts
@@ -1,18 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // eslint-disable-next-line import/order
 import { AxiosResponse } from 'axios';
 import { AccordionListPopulator } from './AccordionListPopulator';
+import FreshToolBox from "./FreshToolBox";
 import IQueryResultProcessor from './IQueryResultProcessor';
-/** @class AccordionListPopulatorResultProcessor */
+/**
+ * @export
+ * @class AccordionListPopulatorResultProcessor
+ * @implements {IQueryResultProcessor}
+ */
 export class AccordionListPopulatorResultProcessor implements IQueryResultProcessor {
     calling_object: AccordionListPopulator;
-    constructor( calling_objectArg: AccordionListPopulator ) {
-        this.calling_object = calling_objectArg;
-    }
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    constructor( calling_objectArg: AccordionListPopulator ) { this.calling_object = calling_objectArg; }
+    /**
+     * @method processQueryResult
+     * @description
+     * The calling object has a list of strings in an array called accordion_list.  This method
+     * adds a section string id to the accordion_list array if it is not already in the array.
+     * @param {AxiosResponse< any >} queryResultToBeProcessed
+     * @return { void }
+     * @memberof AccordionListPopulatorResultProcessor
+     */
     processQueryResult( queryResultToBeProcessed: AxiosResponse< any >): void {
-        for (const element of queryResultToBeProcessed.data) {
-            this.calling_object.accordion_list.push( element.object_view_id );
-        }
-        console.log( "done." );
-    }
+        for ( const element of queryResultToBeProcessed.data ) {
+            element.id = element.object_view_id;  // the fresh tool box will use this as the id.
+            if ( !FreshToolBox.isInArray( element, this.calling_object.accordion_list )) {
+                this.calling_object.accordion_list.push( element.object_view_id ); }}}
 }
--- accordion-list-populator/src/lib/FreshToolBox.ts
+++ accordion-list-populator/src/lib/FreshToolBox.ts
@@ -1,6 +1,3 @@
-/* eslint-disable functional/prefer-readonly-type */
-/* eslint-disable functional/no-throw-statement */
-/* eslint-disable functional/no-class */
 /**
  *  @class FreshToolBox
  *
@@ -20,7 +17,6 @@ export default class FreshToolBox {
      * @return {*}  {Array< unknown >}
      * @memberof FreshToolBox
      */
-    // eslint-disable-next-line functional/no-return-void
     static removeSpecificObjectFromArray ( objectToRemove: unknown, arrayToRemoveItFrom: Array< unknown > ): void {
         arrayToRemoveItFrom.splice( arrayToRemoveItFrom.indexOf( objectToRemove ), 1 ); }
 
@@ -29,7 +25,6 @@ export default class FreshToolBox {
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     static isInArray ( objectToSearchFor: any, arrayToSearch: Array< any > ): boolean {
-        // eslint-disable-next-line functional/no-loop-statement
         for ( const element in arrayToSearch ) {
             if ( arrayToSearch[ element ].id == objectToSearchFor.id ) {
                 return true; }
